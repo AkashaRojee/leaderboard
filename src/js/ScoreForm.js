b464f96cc227b9ad8dcb7467cc95ffb20881ecea
Akasha <Akasha.Rojee@gmail.com>
2021-07-29 18:28:48 +0400

    Fix linter errors

--- src/js/ScoreForm.js
+++ src/js/ScoreForm.js
@@ -12,14 +12,14 @@ export default class ScoreForm {
   submitScore(e, api) {
     e.preventDefault();
     this.createScore();
-    if (Object.values(this).every(value => value !== '')) api.post(this);
+    if (Object.values(this).every((value) => value !== '')) api.post(this);
   }
 
   createScore() {
     const inputValues = document.querySelectorAll('form input');
-    if (Array.from(inputValues).every(inputValue => inputValue.value !== '')) {
+    if (Array.from(inputValues).every((inputValue) => inputValue.value !== '')) {
       [this.user, this.score] = [inputValues[0].value, inputValues[1].value];
-      inputValues.forEach(inputValue => inputValue.value = '');
+      Array.from(inputValues).forEach((inputValue) => { inputValue.value = ''; });
       inputValues[0].focus();
     }
   }
