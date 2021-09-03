ee32c04c895c1d8375b2318f6b8a4bdc30a7adef
Akasha <Akasha.Rojee@gmail.com>
2021-09-03 21:38:36 +0400

    Update build

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import API from './API.js';
 const init = () => {
   const leaderboard = new Leaderboard();
   const scoreForm = new ScoreForm();
-  const api = new API('8sOA1diZKlbkrqVkTAWO');
+  const api = new API('RwkZC4A7KooygrLzIsad');
 
   leaderboard.setEventListener(api);
   scoreForm.setEventListener(leaderboard, api);
